0172fcbfc5b3c74dfa54807e62f863ad1d10556e
pksas <48150087+pksas@users.noreply.github.com>
2019-03-02 17:54:33 +0300

    Исполнено задание 2 по созданию класса Point

--- sandbox/src/main/java/ru/stqa/swtest/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/swtest/sandbox/MyFirstProgram.java
@@ -3,6 +3,10 @@ package ru.stqa.swtest.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
+
+    Point p1 = new Point(4, 3);
+    Point p2 = new Point(0, 0);
+    System.out.println(p1.distance(p2));
     hello("world");
     hello("user");
     hello("Alexey");
---
+++ sandbox/src/main/java/ru/stqa/swtest/sandbox/Point.java
@@ -0,0 +1,15 @@
+package ru.stqa.swtest.sandbox;
+
+public class Point {
+  double x;
+  double y;
+
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public double distance(Point p) {
+    return Math.sqrt((this.x - p.x) * (this.x - p.x) + (this.y - p.y) * (this.y - p.y));
+  }
+}
