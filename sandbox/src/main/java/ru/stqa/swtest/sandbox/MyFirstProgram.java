b3a19d89518802e605b4fb577d1c73020ff22f3e
pksas <48150087+pksas@users.noreply.github.com>
2019-03-02 18:54:09 +0300

    Самостоятельное задание вынесено в отдельный класс

--- sandbox/src/main/java/ru/stqa/swtest/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/swtest/sandbox/MyFirstProgram.java
@@ -4,9 +4,6 @@ public class MyFirstProgram {
 
   public static void main(String[] args) {
 
-    Point p1 = new Point(4, 3);
-    Point p2 = new Point(0, 0);
-    System.out.println(p1.distance(p2));
     hello("world");
     hello("user");
     hello("Alexey");
