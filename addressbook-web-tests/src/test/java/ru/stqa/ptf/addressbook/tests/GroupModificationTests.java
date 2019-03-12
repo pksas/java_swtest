60687ccecf262b619678af55a24c826df5e45d49
pksas <48150087+pksas@users.noreply.github.com>
2019-03-12 20:50:10 +0300

    Добавлен тест на модификацию группы.

---
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.ptf.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.ptf.addressbook.model.GroupData;
+
+public class GroupModificationTests extends TestBase{
+
+  @Test
+  public void testGroupModification() {
+    app.getNavigationHelper().gotoGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("test11", "test22", "test33"));
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returntoGroupPage();
+  }
+}
