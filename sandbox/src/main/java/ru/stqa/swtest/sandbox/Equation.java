39edbd6aec56d802f1b4527bc5a0b6bd414dba87
pksas <48150087+pksas@users.noreply.github.com>
2019-03-15 21:26:10 +0300

    В класс Eqution добавлены проверки на неполные квадратные уравнения. Написаны соответствующие тесты.

--- sandbox/src/main/java/ru/stqa/swtest/sandbox/Equation.java
+++ sandbox/src/main/java/ru/stqa/swtest/sandbox/Equation.java
@@ -11,18 +11,26 @@ public class Equation {
     this.a = a;
     this.b = b;
     this.c = c;
-    double d = b*b - 4*a*c;
 
-    if (d < 0) {
-      n = 0;
-    }
-    else {
-      if (d == 0) {
+    double d = b * b - 4 * a * c;
+
+    if (a != 0) {
+      if (d < 0) {
+        n = 0;
+      } else if (d == 0) {
         n = 1;
-      }
-      else {
+      } else {
         n = 2;
       }
+    } else if (b != 0) {
+      n = 1;
+
+    } else if (c != 0) {
+      n = 0;
+
+    } else {
+      n = -1;
+      
     }
   }
 
