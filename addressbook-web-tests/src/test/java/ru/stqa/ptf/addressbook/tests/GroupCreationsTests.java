63e537e3d600528f2fa800fb13508e1be9fa914c
pksas <48150087+pksas@users.noreply.github.com>
2019-03-16 19:17:22 +0300

    Реализована проверка обеспечения предусловий для модификации/удаления групп.

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/GroupCreationsTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/GroupCreationsTests.java
@@ -8,9 +8,6 @@ public class GroupCreationsTests extends TestBase {
   @Test
   public void testGroupCreations() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
-    app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData("test1", null, null));
-    app.getGroupHelper().submitGroupCreation();
-    app.getGroupHelper().returntoGroupPage();
+    app.getGroupHelper().createGroup(new GroupData("test1", null, null));
   }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/GroupDeletionsTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/GroupDeletionsTests.java
@@ -1,12 +1,16 @@
 package ru.stqa.ptf.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.ptf.addressbook.model.GroupData;
 
 public class GroupDeletionsTests extends TestBase {
 
   @Test
   public void testGroupDeletions() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleyeSelectedGroups();
     app.getGroupHelper().returntoGroupPage();
