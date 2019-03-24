3e210a1c219e0dead86a868358a6a0e0ef9a5a73
pksas <48150087+pksas@users.noreply.github.com>
2019-03-24 13:39:32 +0300

    Добавление проверок на подсчёт количечтва групп.

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/GroupDeletionsTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/GroupDeletionsTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.ptf.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.ptf.addressbook.model.GroupData;
 
@@ -8,12 +9,15 @@ public class GroupDeletionsTests extends TestBase {
   @Test
   public void testGroupDeletions() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
+    int before = app.getGroupHelper().getGroupCount();
     if (! app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("test1", null, null));
     }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleyeSelectedGroups();
     app.getGroupHelper().returntoGroupPage();
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, before - 1);
   }
 
 }
