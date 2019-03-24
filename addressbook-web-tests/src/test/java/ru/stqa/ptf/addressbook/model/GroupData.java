647f2d15d313a2312b11c01e1bcd692d836c1b01
pksas <48150087+pksas@users.noreply.github.com>
2019-03-24 18:53:54 +0300

    Добавлена проверка списка групп для теста создания контакта.

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/model/GroupData.java
@@ -3,12 +3,16 @@ package ru.stqa.ptf.addressbook.model;
 import java.util.Objects;
 
 public class GroupData {
-  private final String id;
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  private int id;
   private final String name;
   private final String header;
   private final String footer;
 
-  public GroupData(String id, String name, String header, String footer) {
+  public GroupData(int id, String name, String header, String footer) {
     this.id = id;
     this.name = name;
     this.header = header;
@@ -16,7 +20,7 @@ public class GroupData {
   }
 
   public GroupData(String name, String header, String footer) {
-    this.id = null;
+    this.id = 0;
     this.name = name;
     this.header = header;
     this.footer = footer;
@@ -26,7 +30,7 @@ public class GroupData {
     return name;
   }
 
-  public String getId() {
+  public int getId() {
     return id;
   }
 
@@ -51,7 +55,7 @@ public class GroupData {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
     GroupData groupData = (GroupData) o;
-    return Objects.equals(id, groupData.id) &&
+    return id == groupData.id &&
             Objects.equals(name, groupData.name);
   }
 
