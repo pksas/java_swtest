be0b418c3e37266410c097aaaeaaa6df8e22217e
pksas <48150087+pksas@users.noreply.github.com>
2019-03-24 19:27:07 +0300

    Реализована сортировка списков в тестах создания и модификации групп.

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/model/GroupData.java
@@ -20,7 +20,7 @@ public class GroupData {
   }
 
   public GroupData(String name, String header, String footer) {
-    this.id = 0;
+    this.id = Integer.MAX_VALUE;
     this.name = name;
     this.header = header;
     this.footer = footer;
@@ -28,6 +28,19 @@ public class GroupData {
 
   public String getName() {
     return name;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    GroupData groupData = (GroupData) o;
+    return Objects.equals(name, groupData.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name);
   }
 
   public int getId() {
@@ -50,17 +63,4 @@ public class GroupData {
             '}';
   }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    GroupData groupData = (GroupData) o;
-    return id == groupData.id &&
-            Objects.equals(name, groupData.name);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, name);
-  }
 }
