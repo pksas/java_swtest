a694230212a2e33caf003f7944287640ef359797
pksas <48150087+pksas@users.noreply.github.com>
2019-03-30 16:14:14 +0300

    Осуществлен поиск максимального элемента в тесте создания контакта с помощью лямьда выражения.

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/ContactsCreationsTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/ContactsCreationsTests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import ru.stqa.ptf.addressbook.model.ContactData;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -14,19 +15,14 @@ public class ContactsCreationsTests extends TestBase{
   public void testContactsCreations() throws Exception {
     app.getNavigationHelper().gotoHomePage();
     List<ContactData> before = app.getContactHelper().getContactList();
-    ContactData contact = new ContactData("test1", "test2", "Ivanovich", "vano", "Ivanovo", "+7(111)2223344", "vano@mail.ru", "test1");
+    ContactData contact = new ContactData("test11", "test2", "Ivanovich", "vano", "Ivanovo", "+7(111)2223344", "vano@mail.ru", "test1");
     app.getNavigationHelper().gotoAddNewContactPage();
     app.getContactHelper().createContact(contact);
     app.getNavigationHelper().gotoHomePage();
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-    int max = 0;
-    for (ContactData c : after) {
-      if (c.getId() > max) {
-        max = c.getId();
-      }
-    }
+    int max = after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId();
     contact.setId(max);
     before.add(contact);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
