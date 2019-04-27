516317073f5feb473fab2306833c86bc3fde553d
pksas <48150087+pksas@users.noreply.github.com>
2019-04-27 23:16:04 +0300

    Реализовано получение почты на встроенный почтовый сервер

--- mantis-tests/out/test/resources/config_inc.php
+++ mantis-tests/out/test/resources/config_inc.php
@@ -7,3 +7,5 @@ $g_db_password = '';
 $g_signup_use_captcha = OFF;
 $g_default_timezone       = 'Europe/Berlin';
 $g_crypto_master_salt     = 'xSY1B/X5xV0Y8jIR0X0H8ol+UJPEeOIlSQgJwxlb/Yo=';
+$g_phpMailer_method = PHPMAILER_METHOD_SMTP;
+$g_smtp_host = 'localhost';
