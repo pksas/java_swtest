a0f2f5926d11793ea0e913cde6a2386f57f532d4
pksas <48150087+pksas@users.noreply.github.com>
2019-04-27 17:30:00 +0300

    Отключена капча, с помощью передачи конфигурационного файла на сервер по протоколу ftp

---
+++ mantis-tests/src/test/resources/config_inc.php
@@ -0,0 +1,9 @@
+<?php
+$g_hostname = 'localhost';
+$g_db_type = 'mysqli';
+$g_database_name = 'bugtracker';
+$g_db_username = 'root';
+$g_db_password = '';
+$g_signup_use_captcha = OFF;
+$g_default_timezone       = 'Europe/Berlin';
+$g_crypto_master_salt     = 'xSY1B/X5xV0Y8jIR0X0H8ol+UJPEeOIlSQgJwxlb/Yo=';
