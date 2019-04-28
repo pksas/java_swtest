226fa81f56833bc5d24aa13e74baa1dcf19add86
pksas <48150087+pksas@users.noreply.github.com>
2019-04-28 16:45:55 +0300

    Реализовано чтение данных пользователей с БД bugtreker с использованием hibernate.

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/model/UserData.java
@@ -0,0 +1,70 @@
+package ru.stqa.pft.mantis.model;
+
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "mantis_user_table")
+public class UserData {
+
+  @Id
+  @Column(name = "id")
+  private int id = Integer.MAX_VALUE;;
+
+  @Column (name = "username")
+  private String username;
+
+  @Column (name = "email")
+  private String email;
+
+  public int getId() {
+    return id;
+  }
+
+  public UserData withId(int id) {
+    this.id = id;
+    return this;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public UserData withUsername(String username) {
+    this.username = username;
+    return this;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public UserData withEmail(String email) {
+    this.email = email;
+    return this;
+  }
+
+  @Override
+  public String toString() {
+    return "UserData{" +
+            "id=" + id +
+            ", username='" + username + '\'' +
+            ", email='" + email + '\'' +
+            '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    UserData userData = (UserData) o;
+    return Objects.equals(username, userData.username) &&
+            Objects.equals(email, userData.email);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(username, email);
+  }
+}
